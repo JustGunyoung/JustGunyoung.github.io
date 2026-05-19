1f33f2ad745c2e212d15b20cc9ccda52b2d7f892
JustGunyoung <park1659@purdue.edu>
2026-05-19 23:04:04 +0900

    personal website0.2

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/globals.css'
+import './styles/animations.css'
+import './styles/overlays.css'
+import './styles/topbar.css'
+import './styles/content.css'
+import './styles/sections.css'
+import './styles/statusbar.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
